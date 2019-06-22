0746f593ec0168f92bbe8f1c7697daa877ee38c9
Dmitriy Kozlov <kdmitriys@gmail.com>
2019-06-22 17:32:49 +0300

    Учтены замечания

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
 'use strict';
 
 (function () {
-  // Объявляем все переменные
+  // Объявляем все переменные и константы
+  var PINS_COUNT = 8;
+  var PINS_COORD_Y_MIN = 130;
+  var PINS_COORD_Y_MAX = 630;
   var mapBlock = document.querySelector('.map'); // Блок с картой
+  var mapPins = document.querySelector('.map__pins'); // родительский элемент для всех пинов на карте
   var pinTemplate = document //
     .querySelector('#pin')
     .content.querySelector('.map__pin');
   var enumAppartmentType = ['palace', 'flat', 'house', 'bungalo'];
-  var adsDataArray = []; // Массив с данными (будем делать из него mock)
-  var mapPins = document.querySelector('.map__pins'); // родительский элемент для всех пинов на карте
 
   // Возвращает случайный элемент массива
   function getRandomArrayElement(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
 
-  // Возвращает объект-заглушку, соответствующий по сруктуре данных реальному бъявлению
+  // Возвращает массив mock-объектов, соответствующих по сруктуре данных реальным объявлениям
   function getMockAdsData() {
-    return {
-      author: {
-        avatar: 'img/avatars/user' + '0' + Math.ceil(Math.random() * 8) + '.png'
-      },
-      offer: {
-        type: getRandomArrayElement(enumAppartmentType),
-        title: 'Заголовок объявления' // свойства с заголовком нет в Задании, но по смыслу оно д.б. тут
-      },
-      location: {
-        x: Math.round(Math.random() * mapBlock.offsetWidth),
-        y: Math.round(Math.random() * 500 + 130)
-      }
-    };
+    var mockPinsArray = [];
+    // массив с данными заполняем mock-объектами
+    for (var i = 0; i < PINS_COUNT; i++) {
+      mockPinsArray[i] = {
+        author: {
+          avatar: 'img/avatars/user' + '0' + Math.ceil(Math.random() * 8) + '.png'
+        },
+        offer: {
+          type: getRandomArrayElement(enumAppartmentType),
+          title: 'Заголовок объявления' // свойства с заголовком нет в Задании, но по смыслу оно д.б. тут
+        },
+        location: {
+          x: Math.round(Math.random() * mapBlock.offsetWidth),
+          y: Math.round(Math.random() * (PINS_COORD_Y_MAX - PINS_COORD_Y_MIN) + PINS_COORD_Y_MIN)
+        }
+      };
+    }
+    return mockPinsArray;
   }
 
   // Создает будущий DOM-элемент из шаблона на основе объекта с данными конкретного объявления
-  function getPinItem(template, pinData) {
-    var newPinNode = template.cloneNode(true);
+  function getPinItem(pinData) {
+    var newPinNode = pinTemplate.cloneNode(true);
     newPinNode.style.left = pinData.location.x + 'px';
     newPinNode.style.top = pinData.location.y + 'px';
     newPinNode.querySelector('img').src = pinData.author.avatar;
@@ -42,23 +49,17 @@
     return newPinNode;
   }
 
-  // В указанный родительский элемент (карту) добавляет Пины на основе данных из массива
-  function appendPinsFromDataArray(parent, dataArray) {
+  // Добавляет на карту пины на основе данных из массива
+  function appendPinsFromDataArray(dataArray) {
     var pinsDocumentFragment = document.createDocumentFragment();
     for (var i = 0; i < dataArray.length; i++) {
-      pinsDocumentFragment.appendChild(getPinItem(pinTemplate, dataArray[i]));
+      pinsDocumentFragment.appendChild(getPinItem(dataArray[i]));
     }
-    parent.appendChild(pinsDocumentFragment);
+    mapPins.appendChild(pinsDocumentFragment);
   }
-
-  // массив с данными заполняем mock-объектами
-  for (var i = 0; i < 8; i++) {
-    adsDataArray[i] = getMockAdsData();
-  }
-
 
   mapBlock.classList.remove('map--faded'); // Визуально переводим карту в активное состояние
-  appendPinsFromDataArray(mapPins, adsDataArray); // Выводим пины на карту
+  appendPinsFromDataArray(getMockAdsData()); // Выводим пины на карту
 })();
 
 // EOF
